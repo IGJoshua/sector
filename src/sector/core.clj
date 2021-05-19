2d675672e2725756ca73b2018c83f73b2256b4f3
Joshua Suskalo <joshua@suskalo.org>
2021-05-19 11:37:07 -0500

    Add basic parser implementation

--- src/sector/core.clj
+++ src/sector/core.clj
@@ -1,1 +1,177 @@
-(ns sector.core)
+(ns sector.core
+  (:require
+   [farolero.core :as far
+    :refer [handler-bind handler-case restart-case
+            values multiple-value-bind multiple-value-list
+            tagbody go block return-from]])
+  (:refer-clojure :rename {comp fn-comp}))
+
+(defmethod far/report-condition ::error
+  [c & {:keys [expected actual]}]
+  (str "Parse error, expected " expected " but was " (pr-str actual)))
+
+;; Parsers are functions from sequences of tokens to multiple values, the first
+;; being the parsed result, the second the sequence of remaining tokens.
+
+(defn eof
+  [coll]
+  (if (seq coll)
+    (far/error ::error
+               :expected "eof"
+               :actual (first coll)
+               :remaining coll)
+    (values
+     nil
+     nil)))
+
+(defn matches
+  ([pred] (matches "to match" pred))
+  ([description pred]
+   (fn [coll]
+     (if (seq coll)
+       (if-some [ret (pred (first coll))]
+         (values ret (rest coll))
+         (far/error ::error
+                    :expected description
+                    :actual (first coll)
+                    :remaining coll))
+       (far/error ::error
+                  :expected description
+                  :actual nil
+                  :remaining nil)))))
+
+(defn many
+  ([parser] (many conj []))
+  ([parser rf init]
+   (fn [coll]
+     (let [ret (volatile! init)
+           remaining (volatile! coll)]
+       (block return
+         (tagbody
+          loop
+          (handler-case
+              (do (when-not (seq @remaining)
+                    (return-from return (values @ret nil)))
+                  (multiple-value-bind [[v r] (parser @remaining)]
+                    (vswap! ret rf v)
+                    (vreset! remaining r)
+                    (go loop)))
+            (::error [c & {:keys [remaining]}]
+              (return-from
+               return
+               (values
+                @ret
+                remaining))))))))))
+
+(defn many1
+  ([parser] (many1 parser conj []))
+  ([parser rf init]
+   (fn [coll]
+     (multiple-value-bind [[ret remaining] (parser coll)]
+       ((many parser rf (rf init ret)) remaining)))))
+
+(defn skip-many
+  [parser]
+  (fn [coll]
+    ((many parser (fn [_ _] nil) nil) coll)
+    nil))
+
+(defn skip-many1
+  [parser]
+  (fn [coll]
+    ((many1 parser (fn [_ _] nil) nil) coll)
+    nil))
+
+(defn comp*
+  [rf init & parsers]
+  (fn [coll]
+    (let [ret (volatile! init)
+          remaining (volatile! coll)]
+      (doseq [parser parsers]
+        (multiple-value-bind [[v r] (parser @remaining)]
+          (vswap! ret rf v)
+          (vreset! remaining r)))
+      (values
+       @ret
+       @remaining))))
+
+(defn comp
+  [& parsers]
+  (apply comp* conj [] parsers))
+
+(defn comp-keys
+  [& parsers]
+  (let [parser-pairs (partition 2 parsers)]
+    (fn [coll]
+      (loop [acc {}
+             coll coll
+             parsers parser-pairs]
+        (if (seq parsers)
+          (let [[k p] (first parsers)
+                [v r] (multiple-value-list (p coll))]
+            (recur (assoc acc k v)
+                   r
+                   (rest parsers)))
+          (values acc coll))))))
+
+(defn alt
+  {:arglists '([& parsers] [description & parsers])}
+  ([& parsers]
+   (let [[description & parsers]
+         (if (string? (first parsers))
+           parsers
+           (cons "one of the options" parsers))]
+     (fn [coll]
+       (block return
+         (doseq [parser parsers]
+           (handler-case (parser coll)
+             (::error [& _])
+             (:no-error [v r] (return-from return (values v r)))))
+         (far/error ::error
+                    :expected description
+                    :actual (first coll)
+                    :remaining coll))))))
+
+(defn lookahead
+  [parser]
+  (fn [coll]
+    (parser coll)
+    (values nil coll)))
+
+(defn lookahead-not
+  ([parser] (lookahead-not "not to succeed at lookahead" parser))
+  ([description parser]
+   (fn [coll]
+     (handler-case (parser coll)
+       (::error [& _] (values nil coll))
+       (:no-error [v r]
+         (far/error ::error
+                    :expected description
+                    :actual (take (- (count coll) (count r)) coll)
+                    :remaining coll))))))
+
+(defn maybe
+  [parser]
+  (fn [coll]
+    (handler-case (parser coll)
+      (::error [& _] (values nil coll)))))
+
+(defn none
+  [coll]
+  (values nil coll))
+
+(defn if-pred
+  [pred]
+  (matches #(if (pred %) % nil)))
+
+(defn parse
+  [parser coll]
+  (handler-case (parser coll)
+    (::error [& args]
+      (println (apply far/report-condition args)))
+    (:no-error [ret remaining]
+      ret)))
+
+(defn parse-all
+  [parser coll]
+  (first (parse (comp parser eof) coll)))
